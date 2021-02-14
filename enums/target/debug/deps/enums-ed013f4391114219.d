a04cbe722031a7349664e94b13d85a2db04a600e
anynoise00 <anynoise00@gmail.com>
2021-02-14 15:46:06 -0300

    Finished Rust book chapter 6

---
+++ enums/target/debug/deps/enums-ed013f4391114219.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/anyno/Documents/Rust Projects/rust-studies/enums/target/debug/deps/enums-ed013f4391114219: src/main.rs
+
+/mnt/c/Users/anyno/Documents/Rust Projects/rust-studies/enums/target/debug/deps/enums-ed013f4391114219.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/anyno/Documents/Rust\ Projects/rust-studies/enums/target/debug/enums: /mnt/c/Users/anyno/Documents/Rust\ Projects/rust-studies/enums/src/main.rs
